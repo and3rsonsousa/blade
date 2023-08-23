c504b020270fb11cd293fd062d971e66aab4aa09
Anderson <and3rsonsousa@outlook.com>
2023-08-22 21:35:09 -0300

    changes

--- app/routes/dashboard._index.tsx
+++ app/routes/dashboard._index.tsx
@@ -9,7 +9,7 @@ import {
 	startOfWeek,
 	toDate,
 } from "date-fns";
-import { Plus } from "lucide-react";
+import { Bold, Italic, Plus, Underline } from "lucide-react";
 import { Button } from "~/components/ui/button";
 import {
 	Dialog,
@@ -32,8 +32,9 @@ import {
 
 import supabaseServer from "~/lib/supabase.server";
 
-import { EditorContent, useEditor } from "@tiptap/react";
+import { BubbleMenu, EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { useState, type ComponentProps, type ReactNode } from "react";
 import { Label } from "~/components/ui/label";
 
 export const meta: V2_MetaFunction = () => [
@@ -61,6 +62,7 @@ export default function DashboardIndex() {
 	const start = startOfWeek(startOfMonth(date));
 	const end = endOfWeek(endOfMonth(date));
 	const days = eachDayOfInterval({ start, end });
+	const [open, setOpen] = useState(false);
 
 	const calendar: CalendarType = [];
 
@@ -83,106 +85,168 @@ export default function DashboardIndex() {
 			"<h2>testando meu deus</h2><p>de novo</p><p>Essa <strong>merda</strong> aqui</p><ol><li><p>Testando</p></li><li><p>valha jesus</p></li></ol><p></p><hr><p>Parece que agora vai</p><p>Sei lá né?</p><p></p>",
 		editorProps: {
 			attributes: {
-				class: "outline-none bg-input p-4 rounded-lg",
+				class: "outline-none bg-input px-4 py-3 rounded-lg",
 			},
 		},
 		onUpdate() {
 			console.log(editor?.getHTML().toString());
 		},
+		onBlur() {
+			console.log("OK");
+		},
 	});
 
 	return (
-		<div
-			className={`sm:grid grid-cols-7 ${
-				calendar.length === 35 ? "grid-rows-5" : "grid-rows-6"
-			} grow sm:overflow-hidden`}
-		>
-			{calendar.map(
-				(c: DayType, i: number) =>
-					c.actions.length > 0 && (
+		<>
+			<div
+				className={`sm:grid grid-cols-7 ${
+					calendar.length === 35 ? "grid-rows-5" : "grid-rows-6"
+				} grow sm:overflow-hidden`}
+			>
+				{calendar.map((c: DayType, i: number) => (
+					<div
+						key={i}
+						className={`px-6 py-2 sm:p-2 sm:flex sm:flex-col sm:overflow-hidden ${
+							c.actions.length === 0 ? " max-sm:hidden " : ""
+						}`}
+					>
 						<div
-							key={i}
-							className="px-6 py-2 sm:p-2 sm:flex sm:flex-col sm:overflow-hidden"
+							className={`text-sm mb-2 ${
+								c.day.getMonth() !== date.getMonth()
+									? "opacity-25"
+									: ""
+							}`}
 						>
-							<div
-								className={`text-sm mb-2 ${
-									c.day.getMonth() !== date.getMonth()
-										? "opacity-25"
-										: ""
-								}`}
-							>
-								{c.day.getDate()}
-							</div>
-							<ScrollArea className="max-h-full grow shrink-0 pb-4">
-								{c.actions.map((action) => (
-									<div
-										key={action.id}
-										className="mb-1 pl-2 border-l-2 border-orange-900 hover:border-orange-700 py-0.5 text-xs hover:bg-card transition cursor-pointer text-slate-500"
-									>
-										{action.title}
-									</div>
-								))}
-							</ScrollArea>
+							{c.day.getDate()}
 						</div>
-					)
-			)}
+						<ScrollArea className="max-h-full grow shrink-0 pb-4">
+							{c.actions.map((action) => (
+								<div
+									key={action.id}
+									className="mb-1 pl-2 border-l-2 border-orange-900 hover:border-orange-700 py-0.5 text-xs hover:bg-card transition cursor-pointer text-slate-500 overflow-hidden text-ellipsis whitespace-nowrap w-full"
+								>
+									{action.title}
+								</div>
+							))}
+						</ScrollArea>
+					</div>
+				))}
 
-			<div className="fixed bottom-6 right-4">
-				<Dialog defaultOpen={true}>
-					<DialogTrigger asChild>
-						<Button size={"icon"} className="rounded-full">
-							<Plus />
-						</Button>
-					</DialogTrigger>
-					<DialogContent>
-						<DialogHeader>
-							<DialogTitle>Nova Ação</DialogTitle>
-						</DialogHeader>
-						<DialogDescription>
-							<ScrollArea className="max-sm:h-[60vh] -m-2">
-								<div className="p-2">
-									<div className="mb-4 space-y-2">
-										<Label htmlFor="title">Título</Label>
-										<Input
-											id="title"
-											name="title"
-											placeholder="Título da Ação"
-										/>
-									</div>
-									<div className="mb-4 space-y-2">
-										<Label htmlFor="title">Categoria</Label>
-										<Select>
-											<SelectTrigger>
-												<SelectValue placeholder="Categoria" />
-											</SelectTrigger>
-											<SelectContent>
-												<SelectItem value="1">
-													Light
-												</SelectItem>
-												<SelectItem value="2">
-													Dark
-												</SelectItem>
-												<SelectItem value="3">
-													System
-												</SelectItem>
-											</SelectContent>
-										</Select>
-									</div>
-									<div className="mb-4 space-y-2">
-										<Label htmlFor="title">Descrição</Label>
-										<div className="prose">
-											<EditorContent editor={editor} />
+				<div className="fixed bottom-6 right-4">
+					<Dialog defaultOpen={true}>
+						<DialogTrigger asChild>
+							<Button size={"icon"} className="rounded-full">
+								<Plus />
+							</Button>
+						</DialogTrigger>
+						<DialogContent
+							onInteractOutside={(event) => {
+								if (open) {
+									event.preventDefault();
+								}
+							}}
+						>
+							<DialogHeader>
+								<DialogTitle>Nova Ação</DialogTitle>
+							</DialogHeader>
+							<DialogDescription>
+								<ScrollArea className="h-[60vh] -m-2">
+									<div className="p-2">
+										<div className="mb-4 space-y-2">
+											<Label htmlFor="title">
+												Título
+											</Label>
+											<Input
+												id="title"
+												name="title"
+												placeholder="Título da Ação"
+											/>
+										</div>
+										<div className="mb-4 space-y-2">
+											<Label htmlFor="title">
+												Categoria
+											</Label>
+											<Select>
+												<SelectTrigger>
+													<SelectValue placeholder="Escolha a Categoria da Ação" />
+												</SelectTrigger>
+												<SelectContent>
+													<SelectItem value="1">
+														Light
+													</SelectItem>
+													<SelectItem value="2">
+														Dark
+													</SelectItem>
+													<SelectItem value="3">
+														System
+													</SelectItem>
+												</SelectContent>
+											</Select>
+										</div>
+										<div className="mb-4 space-y-2">
+											<Label htmlFor="title">
+												Descrição
+											</Label>
+											<div className="prose">
+												<EditorContent
+													editor={editor}
+												/>
+											</div>
 										</div>
 									</div>
-								</div>
-							</ScrollArea>
-						</DialogDescription>
-						<DialogFooter>
-							<Button>Inserir</Button>
-						</DialogFooter>
-					</DialogContent>
-				</Dialog>
+								</ScrollArea>
+							</DialogDescription>
+							<DialogFooter>
+								<Button>Inserir</Button>
+							</DialogFooter>
+						</DialogContent>
+					</Dialog>
+				</div>
 			</div>
-		</div>
+			{editor && (
+				<BubbleMenu
+					editor={editor}
+					className="bg-accent shadow-lg border border-muted shadow-black flex  rounded-lg m-0 overflow-hidden"
+				>
+					<BubleButton
+						data-active={editor.isActive("bold")}
+						onClick={(event) => {
+							event.preventDefault();
+							event.stopPropagation();
+							editor.chain().focus().toggleBold().run();
+							return false;
+						}}
+					>
+						<Bold className="w-4 h-4" />
+					</BubleButton>
+					<BubleButton
+						data-active={editor.isActive("italic")}
+						onClick={(event) => {
+							editor.chain().focus().toggleItalic().run();
+						}}
+					>
+						<Italic className="w-4 h-4" />
+					</BubleButton>
+
+					<BubleButton
+						data-active={editor.isActive("strike")}
+						onClick={() => {
+							editor.chain().focus().toggleStrike().run();
+						}}
+					>
+						<Underline className="w-4 h-4" />
+					</BubleButton>
+				</BubbleMenu>
+			)}
+		</>
 	);
 }
+
+const BubleButton = (
+	props: ComponentProps<"button"> & { children: ReactNode }
+) => (
+	<button
+		className="h-8 w-10 text-muted-foreground grid place-content-center hover:bg-input data-[active=true]:bg-muted data-[active=true]:text-muted-foreground"
+		{...props}
+	></button>
+);
