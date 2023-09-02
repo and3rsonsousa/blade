c00d61d405b8bb6c8d55c990ef0b2926ca9b1f63
Anderson <and3rsonsousa@outlook.com>
2023-09-02 19:12:18 -0300

    edge 1

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,42 +1,39 @@
-import { type LoaderArgs, redirect, json } from "@remix-run/node";
+import { type LoaderArgs, redirect, json } from "@vercel/remix";
+// import { type LoaderArgs, redirect, json } from "@remix-run/node";
 import { Outlet, useLoaderData } from "@remix-run/react";
 import Layout from "~/components/structure/layout";
 import supabaseServer from "~/lib/supabase.server";
 
+export const config = { runtime: "edge" };
+
 export async function loader({ request }: LoaderArgs) {
-	const response = new Response();
-	const supabase = supabaseServer({
-		request,
-		response,
-	});
+  const response = new Response();
+  const supabase = supabaseServer({
+    request,
+    response,
+  });
 
-	const {
-		data: { session },
-	} = await supabase.auth.getSession();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
 
-	if (!session) return redirect("/login", { headers: response.headers });
+  if (!session) return redirect("/login", { headers: response.headers });
 
-	const [{ data: categories }, { data: clients }, { data: states }] =
-		await Promise.all([
-			supabase
-				.from("categories")
-				.select("*")
-				.order("id", { ascending: true }),
-			supabase
-				.from("clients")
-				.select("*")
-				.order("title", { ascending: true }),
-			supabase.from("states").select("*").order("order"),
-		]);
+  const [{ data: categories }, { data: clients }, { data: states }] =
+    await Promise.all([
+      supabase.from("categories").select("*").order("id", { ascending: true }),
+      supabase.from("clients").select("*").order("title", { ascending: true }),
+      supabase.from("states").select("*").order("order"),
+    ]);
 
-	return json({ categories, clients, states });
+  return json({ categories, clients, states });
 }
 
 export default function Dashboard() {
-	const context = useLoaderData<typeof loader>();
-	return (
-		<Layout>
-			<Outlet context={{ ...context }} />
-		</Layout>
-	);
+  const context = useLoaderData<typeof loader>();
+  return (
+    <Layout>
+      <Outlet context={{ ...context }} />
+    </Layout>
+  );
 }
