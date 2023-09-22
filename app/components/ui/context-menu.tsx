784c9bfe279c9100161b0ca8832a125be5e3969a
Anderson <and3rsonsousa@outlook.com>
2023-09-22 17:26:33 -0300

    home and context menu

--- app/components/ui/context-menu.tsx
+++ app/components/ui/context-menu.tsx
@@ -25,7 +25,7 @@ const ContextMenuSubTrigger = React.forwardRef<
   <ContextMenuPrimitive.SubTrigger
     ref={ref}
     className={cn(
-      "flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none data-[state=open]:bg-secondary/50",
+      "flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none data-[state=open]:bg-accent",
       inset && "pl-8",
       className,
     )}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,8 +6,20 @@ import { Button } from "~/components/ui/button";
 
 export default function Index() {
   return (
-    <div className="grid min-h-screen place-content-center">
-      <div className="max-w-xs text-center">
+    <div className="relative grid min-h-screen place-content-center">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 3 }}
+        className="absolute left-1/2 top-1/3 z-0 h-[40vh] w-[80vh] -translate-x-1/2 rounded-full bg-primary opacity-50 blur-[200px]"
+      ></motion.div>
+      <motion.div
+        initial={{ opacity: 0, scale: 0.8, x: "-50%" }}
+        animate={{ opacity: 1, scale: 0.8, x: "-50%" }}
+        transition={{ duration: 2 }}
+        className="absolute left-1/2 top-2/3 z-0 h-[20vh] w-[40vh] -translate-x-1/2 rounded-full bg-primary blur-[80px]"
+      ></motion.div>
+      <div className="relative max-w-xs text-center">
         {/* <div className="inline-block bg-gradient-to-r from-teal-400 via-primary  via-[65%] to-rose-500 bg-clip-text text-5xl font-black text-transparent">
           BLADE
         </div> */}
@@ -18,11 +30,11 @@ export default function Index() {
             exit={{ opacity: 0, scale: 0.9 }}
             transition={{ duration: 2, ease: [0.215, 0.61, 0.355, 1.0] }}
           >
-            <Blade className="mx-auto w-48" />
+            <Blade className="z-10 mx-auto w-48" />
           </motion.div>
 
           <motion.div
-            className="my-8"
+            className="z-10 my-8"
             initial={{ opacity: 0, scale: 1.2 }}
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.9 }}
@@ -51,6 +63,7 @@ export default function Index() {
               ease: [0.215, 0.61, 0.355, 1.0],
               delay: 1,
             }}
+            className="z-10"
           >
             <Button variant={"default"} asChild>
               <Link to="/dashboard">
